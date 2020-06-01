2281af5598e4c1b00792664a186b30ff2ffc1f74
Brollof <thefish3r@gmail.com>
2020-06-01 19:19:39 +0200

    moving mouse automatically

--- amm/inc/Main.h
+++ amm/inc/Main.h
@@ -1,11 +1,21 @@
 #pragma once
 
 #include "wx/wx.h"
+#include <wx/uiaction.h>
 
 class Main : public wxFrame
 {
 public:
   Main(std::string appName);
   ~Main();
+
+private:
+  void OnTimer(wxTimerEvent& event);
+  int m_timCnt = 0;
+  int m_dir = 1;
+  wxTimer m_timer;
+  wxPoint m_lastMousePos;
+  wxUIActionSimulator m_sim;
+  // wxDECLARE_EVENT_TABLE();
 
 };
--- amm/src/Main.cpp
+++ amm/src/Main.cpp
@@ -5,6 +5,8 @@
 
 #define APP_WIDTH 200
 #define APP_HEIGHT 100
+#define MOUSE_IDLE_TIME 5000
+#define TIMER_PERIOD 500
 
 
 Main::Main(std::string appName) : wxFrame(nullptr, wxID_ANY, appName)
@@ -18,6 +20,8 @@ Main::Main(std::string appName) : wxFrame(nullptr, wxID_ANY, appName)
   this->SetWindowStyle(this->GetWindowStyle() & ~(wxMAXIMIZE_BOX | wxRESIZE_BORDER));
   this->SetIcon(wxICON(MAIN_ICON));
 
+  m_timer.Bind(wxEVT_TIMER, &Main::OnTimer, this);
+  m_timer.Start(TIMER_PERIOD);
 }
 
 Main::~Main()
@@ -25,3 +29,31 @@ Main::~Main()
 
 }
 
+std::ostream& operator<<(std::ostream& os, const wxPoint& pt)
+{
+  os << "{" << pt.x << "," << pt.y << "}";
+  return os;
+}
+
+void Main::OnTimer(wxTimerEvent& event)
+{
+  wxPoint pt = wxGetMousePosition();
+  std::cout << pt << std::endl;
+
+  // Mouse has moved
+  if (pt != m_lastMousePos)
+  {
+    m_timCnt = 0;
+  }
+  else
+  {
+    m_timCnt++;
+    if (m_timCnt * TIMER_PERIOD >= MOUSE_IDLE_TIME) // move mouse manually
+    {
+      m_dir *= -1; // invert direction
+      m_sim.MouseMove({m_lastMousePos.x + m_dir, m_lastMousePos.y});
+      std::cout << "moving mouse, dir: " << m_dir << std::endl;
+    }
+  }
+  m_lastMousePos = pt;
+}
