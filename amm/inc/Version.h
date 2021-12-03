f7d1dc5d99f07aa466b64cdc618afe53642adeb7
Brollof <thefish3r@gmail.com>
2021-12-03 18:50:32 +0100

    version updated to 1.1.2

--- amm/inc/Version.h
+++ amm/inc/Version.h
@@ -7,7 +7,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               1
-#define VERSION_REVISION            1
+#define VERSION_REVISION            2
 
 #define VER_FILE_DESCRIPTION_STR    "amm"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
